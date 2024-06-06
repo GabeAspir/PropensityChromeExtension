37965192fbd199b36cbc842bd57c11cc5670758f
Gabriel Aspir <gaspir@mail.yu.edu>
2024-06-06 19:45:48 -0400

    Almost done with account, dont know why the links arent centering...

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-anychart.onDocumentReady(function () {
-	var data = [
-		["January", 1000],
-		["February", 1200],
-		["March", 1800],
-		["April", 1500],
-		["May", 1600],
-		["June", 1900],
-	];
-	var chart = anychart.line();
-	var lineSeries = chart.line(data);
-	chart.container("chartContainer");
-	chart.draw();
-});
+// anychart.onDocumentReady(function () {
+// 	var data = [
+// 		["January", 1000],
+// 		["February", 1200],
+// 		["March", 1800],
+// 		["April", 1500],
+// 		["May", 1600],
+// 		["June", 1900],
+// 	];
+// 	var chart = anychart.line();
+// 	var lineSeries = chart.line(data);
+// 	chart.container("chartContainer");
+// 	chart.draw();
+// });
 $(document).ready(function() {
 	$('.nav-item .nav-link').on('click', function(event) {
 		event.preventDefault();
