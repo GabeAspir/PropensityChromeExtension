f216c13de8f87e7f34e73562b6225b99d8938f90
Gabriel Aspir <gaspir@mail.yu.edu>
2024-06-27 15:47:04 -0400

    Second chart is finished

--- js/main.js
+++ js/main.js
@@ -67,3 +67,23 @@ anychart.onDocumentReady(function () {
 	chart.legend().enabled(false)
 	chart.draw();
 });
+
+anychart.onDocumentReady(function () {
+	var data = [
+		{x: "Mar 1", value: 0},
+		{x: "Mar 8", value: 2},
+		{x: "Mar 15", value: 1},
+		{x: "Mar 22", value: 0},
+		{x: "Mar 30", value: 1},
+		{x: "Apr 06", value: 2},
+		{x: "Apr 13", value: 0}
+	];
+
+	var chart = anychart.area();
+	var series = chart.splineArea(data);
+	series.fill('#AA4CFC 0.1');
+	series.stroke('#AA4CFC');
+	chart.yAxis().enabled(false);
+	chart.container("splineChartContainer");
+	chart.draw();
+});
